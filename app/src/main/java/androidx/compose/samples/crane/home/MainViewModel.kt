8c07ba6227662c11b98d6eb186d00ff8c7a4f900
Hsu Yee Thin <hsuyeethin@rebbiz.com>
2025-01-29 21:39:32 +0630

    only finish to step 8 need to continue

--- app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
+++ app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
@@ -23,6 +23,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -38,6 +41,13 @@ class MainViewModel @Inject constructor(
 
     val hotels: List<ExploreModel> = destinationsRepository.hotels
     val restaurants: List<ExploreModel> = destinationsRepository.restaurants
+    private val _suggestedDestinations = MutableStateFlow<List<ExploreModel>>(emptyList())
+
+    val suggestedDestinations: StateFlow<List<ExploreModel>> = _suggestedDestinations.asStateFlow()
+
+    init {
+        _suggestedDestinations.value = destinationsRepository.destinations
+    }
 
     fun updatePeople(people: Int) {
         viewModelScope.launch {
